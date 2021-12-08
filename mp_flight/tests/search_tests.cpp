b0446b1fa65ae2b6752d3d4adfd552aca1235aa4
meganaadi <madiga2@illinois.edu>
2021-12-08 00:33:16 -0600

    basic test cases fixed. yay! dijkstras basic works

--- mp_flight/tests/search_tests.cpp
+++ mp_flight/tests/search_tests.cpp
@@ -37,8 +37,103 @@ TEST_CASE("Testing_Dijkstra_On_Graph", "[weight=15]") {
     graph.addRoute("2", "4", 6);
     graph.addRoute("5", "1", 5);
     graph.addRoute("5", "3", 32);
-    //run djkstras + verify
+    //run djkstras + verify 1
     Search_Dijkstra sd;
     std::vector<const Route*> v = sd.searchMyFlight(graph, "1", "5");
-    REQUIRE(v[0]->getDistance() == 5); 
+    std::cout << "this is a test" << std::endl;
+    REQUIRE(v.size() == 1); 
+    //run djkstras + verify 2
+    v = sd.searchMyFlight(graph, "1", "4");
+    std::cout << "this is a test 2" << std::endl;
+    REQUIRE(v.size() == 2); 
+    //run djkstras + verify 3
+    v = sd.searchMyFlight(graph, "2", "3");
+    std::cout << "this is a test 3" << std::endl;
+    REQUIRE(v.size() == 3); 
+}
+ /**
+TEST_CASE("Testing_Dijkstra_On_Graph_2", "[weight=15]") {
+    //create graph (truetrue)
+    Graph graph = Graph(true, true);
+    //create airports + add airports
+    Airport airport = Airport("Chicago","USA","1",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("Seattle","USA","2",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("megana","USA","3",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("molly","USA","4",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("swarna","USA","5",0.0,0.0);
+    graph.addAirport(airport);
+    //add routes with codes and distance 
+    graph.addRoute("1", "5", 5);
+    graph.addRoute("1", "2", 1);
+    graph.addRoute("4", "5", 12);
+    graph.addRoute("3", "5", 7);
+    graph.addRoute("2", "4", 6);
+    graph.addRoute("5", "1", 5);
+    graph.addRoute("5", "3", 32);
+    //run djkstras + verify
+    Search_Dijkstra sd;
+    std::vector<const Route*> v = sd.searchMyFlight(graph, "1", "4");
+    std::cout << "this is a test 2" << std::endl;
+    REQUIRE(v.size() == 2); 
+} 
+
+TEST_CASE("Testing_Dijkstra_On_Graph_3", "[weight=15]") {
+    //create graph (truetrue)
+    Graph graph = Graph(true, true);
+    //create airports + add airports
+    Airport airport = Airport("Chicago","USA","1",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("Seattle","USA","2",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("megana","USA","3",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("molly","USA","4",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("swarna","USA","5",0.0,0.0);
+    graph.addAirport(airport);
+    //add routes with codes and distance 
+    graph.addRoute("1", "5", 5);
+    graph.addRoute("1", "2", 1);
+    graph.addRoute("4", "5", 12);
+    graph.addRoute("3", "5", 7);
+    graph.addRoute("2", "4", 6);
+    graph.addRoute("5", "1", 5);
+    graph.addRoute("5", "3", 32);
+    //run djkstras + verify
+    Search_Dijkstra sd;
+    std::vector<const Route*> v = sd.searchMyFlight(graph, "2", "3");
+    std::cout << "this is a test 3" << std::endl;
+    REQUIRE(v.size() == 3); 
+} **/
+
+TEST_CASE("Testing_Dijkstra_But_No_Route", "[weight=15]") {
+    //create graph (truetrue)
+    Graph graph = Graph(true, true);
+    //create airports + add airports
+    Airport airport = Airport("Chicago","USA","1",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("Seattle","USA","2",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("megana","USA","3",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("molly","USA","4",0.0,0.0);
+    graph.addAirport(airport);
+    airport = Airport("swarna","USA","5",0.0,0.0);
+    graph.addAirport(airport);
+    //add routes with codes and distance 
+    graph.addRoute("1", "5", 5);
+    graph.addRoute("1", "2", 1);
+    graph.addRoute("4", "5", 12);
+    graph.addRoute("3", "5", 7);
+    graph.addRoute("2", "4", 6);
+    graph.addRoute("5", "1", 5);
+    //run djkstras + verify
+    Search_Dijkstra sd;
+    std::vector<const Route*> v = sd.searchMyFlight(graph, "2", "3");
+    std::cout << "this is a test 4" << std::endl;
+    REQUIRE(v.size() == 0); 
 }
